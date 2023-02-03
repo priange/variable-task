b9473b0724f1853a22eff11745bb2f3ce5b9a2a2
priange <ityakup@gmail.com>
2023-02-03 08:10:23 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import com.sun.tools.attach.AgentInitializationException
+
+fun main(args: Array<String>) {
+    var FullName = "Ityaku Priscila Evangelin"
+    println(FullName)
+    var Age = 25
+    println(Age)
+    var contact = 772562598
+    println(contact)
+    var weightinKg: Float = 68.8F
+    println(weightinKg)
+    var citizen: Boolean = false
+    println(citizen)
+
+
+
+
+
+
+}
